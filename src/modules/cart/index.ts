ed0d5cae4a3e917978e9b42015b1ab6da00c984d
Rodrigo Opalo Balog <r.opalo@icloud.com>
2026-04-22 09:39:20 -0400

    feat: add client-side cart editing

--- src/modules/cart/index.ts
+++ src/modules/cart/index.ts
@@ -1,3 +1,9 @@
 export type { CartLine, CartLineCustomization } from "./cart-line";
 export { createCartLineFromSelection } from "./cart-line";
-export { addCartLine, getCartLines } from "./cart-storage";
+export {
+  addCartLine,
+  getCartLines,
+  getCartTotal,
+  removeCartLine,
+  updateCartLineQuantity,
+} from "./cart-storage";
